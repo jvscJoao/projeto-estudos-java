e06eba4f718c783b778ae2724b07083c02601f82
jvscJoao <joaov1500@gmail.com>
2024-08-26 17:37:45 -0300

    first commit

---
+++ src/main/java/com/projeto/estudos/projeto_estudos_java/ProjetoEstudosJavaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.estudos.projeto_estudos_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEstudosJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEstudosJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/estudos/projeto_estudos_java/ProjetoEstudosJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.estudos.projeto_estudos_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEstudosJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
